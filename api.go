cf4197d5c65840ab654b8c7fae2d6e05e79abb47
BoryslavGlov <boryslav.ziubrytskyi@providers.glovoapp.com>
2023-10-16 22:25:46 +0300

    fix

--- api.go
+++ api.go
@@ -6,11 +6,11 @@ import (
 	"encoding/json"
 	"fmt"
 
+	"github.com/boryslavqq/near-api-go/account"
+	itypes "github.com/boryslavqq/near-api-go/internal/types"
+	"github.com/boryslavqq/near-api-go/types"
+	"github.com/boryslavqq/near-api-go/util"
 	"github.com/ethereum/go-ethereum/rpc"
-	"github.com/textileio/near-api-go/account"
-	itypes "github.com/textileio/near-api-go/internal/types"
-	"github.com/textileio/near-api-go/types"
-	"github.com/textileio/near-api-go/util"
 )
 
 // CallFunctionResponse holds information about the result of a function call.
